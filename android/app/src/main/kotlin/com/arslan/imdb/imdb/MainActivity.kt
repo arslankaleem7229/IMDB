11124567c90a0f032c24b83a87c74defe8aa6445
arslankaleem7229 <arslankaleem7229@gmail.com>
2021-03-30 22:26:39 +0500

    mini IMDB
    IMDB like app for learning concepts

---
+++ android/app/src/main/kotlin/com/arslan/imdb/imdb/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.arslan.imdb.imdb
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/movielistview.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MovieListView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Movies"),
+        backgroundColor: Colors.blueGrey.shade900,
+      ),
+    );
+  }
+}
