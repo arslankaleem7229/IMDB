80fb09e8b0945967c5c551a1c35cb0858478ac43
arslankaleem7229 <arslankaleem7229@gmail.com>
2021-03-31 02:08:26 +0500

    applied changes

--- lib/models/movie.dart
+++ lib/models/movie.dart
@@ -1,46 +1,67 @@
 class Movie {
-  String title;
-  String year;
-  String rated;
-  String released;
-  String runtime;
-  String genre;
-  String director;
-  String writer;
-  String actors;
-  String plot;
-  String language;
-  String country;
-  String awards;
-  String poster;
-  String metascore;
-  String imdbrating;
-  String imdbvotes;
-  String imdbid;
-  String type;
-  String response;
-  List<String> images;
-  Movie({
-    this.title,
-    this.year,
-    this.rated,
-    this.released,
-    this.runtime,
-    this.genre,
-    this.director,
-    this.writer,
-    this.actors,
-    this.plot,
-    this.language,
-    this.country,
-    this.awards,
-    this.poster,
-    this.metascore,
-    this.imdbrating,
-    this.imdbvotes,
-    this.imdbid,
-    this.type,
-    this.response,
-    this.images,
-  });
+  String title = "Title";
+  String year = "Year";
+  String rated = "Rated";
+  String released = "Released";
+  String runtime = "Runtime";
+  String genre = "Genre";
+  String director = "Director";
+  String writer = "Writer";
+  String actors = "Actors";
+  String plot = "Plot";
+  String language = "Language";
+  String country = "Country";
+  String awards = "Awards";
+  String poster = "Poster";
+  String metascore = "Metascore";
+  String imdbrating = "imdbRating";
+  String imdbvotes = "imdbVotes";
+  String imdbid = "imdbID";
+  String type = "Type";
+  String response = "Response";
+  // Movie({
+  //   this.title,
+  //   this.year,
+  //   this.rated,
+  //   this.released,
+  //   this.runtime,
+  //   this.genre,
+  //   this.director,
+  //   this.writer,
+  //   this.actors,
+  //   this.plot,
+  //   this.language,
+  //   this.country,
+  //   this.awards,
+  //   this.poster,
+  //   this.metascore,
+  //   this.imdbrating,
+  //   this.imdbvotes,
+  //   this.imdbid,
+  //   this.type,
+  //   this.response,
+  //   this.images,
+  // });
+  // title: movies[index]["Title"],
+  // year: movies[index]["Year"],
+  // rated: movies[index]["Rated"],
+  // released: movies[index]["Released"],
+  // runtime: movies[index]["Runtime"],
+  // genre: movies[index]["Genre"],
+  // director: movies[index]["Director"],
+  // writer: movies[index]["Writer"],
+  // actors: movies[index]["Actors"],
+  // plot: movies[index]["Plot"],
+  // language: movies[index]["Language"],
+  // country: movies[index]["Country"],
+  // awards: movies[index]["Awards"],
+  // poster: movies[index]["Poster"],
+  // metascore: movies[index]["Metascore"],
+  // imdbrating: movies[index]["imdbRating"],
+  // imdbvotes: movies[index]["imdbVotes"],
+  // imdbid: movies[index]["imdbID"],
+  // type: movies[index]["Type"],
+  // response: movies[index]["Response"],
+  // images: movies[index]["Images"],
+
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:imdb/models/movie.dart';
 
+Movie movieData = Movie();
+
 class MovieListView extends StatefulWidget {
   @override
   _MovieListViewState createState() => _MovieListViewState();
@@ -16,13 +18,13 @@ class _MovieListViewState extends State<MovieListView> {
     readJson();
   }
 
-  List movies = [];
+  List _movies = [];
 
   Future<void> readJson() async {
     final String response = await rootBundle.loadString('assets/movies.json');
     final data = await json.decode(response);
     setState(() {
-      movies = data["movies"];
+      _movies = data["movies"];
     });
   }
 
@@ -35,7 +37,7 @@ class _MovieListViewState extends State<MovieListView> {
       ),
       backgroundColor: Colors.blueGrey.shade400,
       body: ListView.builder(
-        itemCount: movies.length,
+        itemCount: _movies.length,
         itemBuilder: (context, index) {
           return Card(
             elevation: 4.5,
@@ -43,22 +45,25 @@ class _MovieListViewState extends State<MovieListView> {
             child: ListTile(
               leading: CircleAvatar(
                 child: Container(
-                  child: Text("M"),
                   decoration: BoxDecoration(
-                    // color: Colors.blue,
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: getImage(index),
+                    ),
                     borderRadius: BorderRadius.circular(45),
                   ),
                 ),
               ),
-              title: Text(movies[index]["Title"]),
-              subtitle: Text(movies[index]["Released"]),
+              title: Text(_movies[index]["Title"]),
+              subtitle: Text(_movies[index]["Released"]),
               trailing: Text("$index"),
               onTap: () => Navigator.push(
                 context,
                 MaterialPageRoute(
                   builder: (context) => MovieListViewDetails(
-                    movieName: movies[index]["Title"],
-                    movie: movies[index],
+                    movieName: _movies[index]["Title"],
+                    movie: _movies,
+                    index: index,
                   ),
                 ),
               ),
@@ -68,12 +73,24 @@ class _MovieListViewState extends State<MovieListView> {
       ),
     );
   }
+
+  getImage(int index) {
+    NetworkImage posterData;
+    posterData = NetworkImage(_movies[index][movieData.poster]);
+    if (posterData != null) {
+      return posterData;
+    } else {
+      posterData = NetworkImage(_movies[index]["Images"][0]);
+      return posterData;
+    }
+  }
 }
 
 class MovieListViewDetails extends StatelessWidget {
   final String movieName;
-  final Movie movie;
-  MovieListViewDetails({this.movieName, this.movie});
+  final List<dynamic> movie;
+  final int index;
+  MovieListViewDetails({this.movieName, this.movie, this.index});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -85,7 +102,7 @@ class MovieListViewDetails extends StatelessWidget {
         child: Container(
           child: ElevatedButton(
             onPressed: () => Navigator.pop(context),
-            child: Text("GO BACK"),
+            child: Text(movie[index][movieData.title]),
           ),
         ),
       ),
